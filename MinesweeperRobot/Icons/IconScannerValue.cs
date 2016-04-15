4377942461f26102a9d6584d93dd18d43b4b2250
tommyinb <tommyinb@yahoo.com.hk>
2016-04-15 18:31:38 +0800

    Scanner Value

---
+++ MinesweeperRobot/Icons/IconScannerValue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinesweeperRobot.Icons
+{
+    public struct IconScannerValue
+    {
+        public uint Pixel;
+
+        public Icon[] Icons;
+    }
+}
